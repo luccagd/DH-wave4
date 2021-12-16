9e4546c549586186e7f31099c699b0b0f2b02401
Lucca Gabriel Dias Barbosa <lucca.dias@mercadolivre.com>
2021-12-16 17:01:09 -0300

    Módulo 3 - Pratica Integradora 1

--- ModuloTres/ExercicioCinco.java
+++ ModuloTres/ExercicioCinco.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
 
-public class ExercicioDois {
-    public static void main(String args[]) {
+public class ExercicioCinco {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int n = scanner.nextInt();
         int m = scanner.nextInt();
         int d = scanner.nextInt();
 
-        int total = 0;
-        for (int i = 1; total < n; i++) {
-            
+        int totalPrintado = 0;
+        String digitoASerVerificado = String.valueOf(d);
+        for (int i = 1; totalPrintado < n; i++) {
+            String numero = String.valueOf(i);
+
+            int totalDeDigitosD = 0;
+            for (int j = 0; j < numero.length(); j++) {
+                if (Character.toString(numero.charAt(j)).equals(digitoASerVerificado)) {
+                    totalDeDigitosD++;
+                }
+
+                if (totalDeDigitosD == m) {
+                    System.out.println(numero);
+                    totalPrintado++;
+                    break;
+                }
+            }
         }
     }
 }
